97491db333c60dbd06e2cb6905243a4ff59828cd
BwayCer <bwaycer@gmail.com>
2019-12-11 14:00:34 +0800

    chore: 判斷是否更動過再決定是否儲存 redis 資料的更改

--- src/utils/chanData.py
+++ src/utils/chanData.py
@@ -19,6 +19,7 @@ _rjdb = rejson.Client(
     port = _env['redisService']['port'],
     decode_responses = True
 )
+_ysUpdate = False
 
 
 class ChanData():
@@ -31,10 +32,48 @@ class ChanData():
 
             _rjdb.jsonset(_redisKey, rejson.Path.rootPath(), data)
 
-    def store(self) -> bool:
-        data = self.get('.')
-        utils.json.dump(data, _chanDataFilePath)
-        return True
+    # 想參考 https://github.com/SPSCommerce/redlock-py
+    # 但執行 `dlm.lock(_redisKey, timeout)` 時會拋出以下錯誤
+    #   ERROR:root:Error unlocking resource tg_niren
+    #     in server Redis<ConnectionPool<Connection<host=localhost,port=6379,db=0>>>
+    #   redis.exceptions.ResponseError: Error running script
+    #     (call to f_4e92fdc63815b3a0a2dd1c1df3694de73607c88d): @user_script:2:
+    #     WRONGTYPE Operation against a key holding the wrong kind of value
+    def dFakeLockSet(memberPath: str = '.', ysStore: bool = False, maxTimes: int = -1):
+        def realDecorator(fn):
+            def _getSafe(memberPath: str) -> typing.Any:
+                try:
+                    data = _rjdb.jsonget(_redisKey, rejson.Path(memberPath))
+                except:
+                    data = None
+                return data
+
+            def newFn(*args) -> bool:
+                times = 0
+                while True:
+                    originData = _getSafe(memberPath)
+                    newData = fn(*args)
+                    # Python 的比較其內容，與 JS 比較其物件指針不同
+                    if originData == _getSafe(memberPath):
+                        _rjdb.jsonset(_redisKey, rejson.Path(memberPath), newData)
+                        _ysUpdate = True
+                        if ysStore:
+                            _ysUpdate = False
+                            rootData = _rjdb.jsonget(_redisKey, rejson.Path.rootPath())
+                            utils.json.dump(rootData, _chanDataFilePath)
+                        return True
+
+                    if maxTimes != -1:
+                        times += 1
+                        if times >= maxTimes:
+                            return False
+            return newFn
+        return realDecorator
+
+    def store(self):
+        if _ysUpdate:
+            rootData = _rjdb.jsonget(_redisKey, rejson.Path.rootPath())
+            utils.json.dump(rootData, _chanDataFilePath)
 
     def opt(self, method: str, memberPath: str, *args) -> typing.Any:
         methodFn = getattr(_rjdb, method)
@@ -42,7 +81,9 @@ class ChanData():
             return methodFn(_redisKey, rejson.Path(memberPath))
         else:
             value = args[0]
-            return methodFn(_redisKey, rejson.Path(memberPath), value)
+            result = methodFn(_redisKey, rejson.Path(memberPath), value)
+            _ysUpdate = True
+            return result
 
     def set(self, memberPath: str, value: typing.Any) -> bool:
         ysSet = self.opt('jsonset', memberPath, value)
