72a736b85a94f12247f41a9b9f9fd6b1fd132c46
BwayCer <bwaycer@gmail.com>
2019-12-11 14:31:43 +0800

    chore: 挑選手機號碼功能

--- src/tgkream/tgTool.py
+++ src/tgkream/tgTool.py
@@ -3,10 +3,174 @@
 
 from typing import Union
 import os
+import datetime
+import random
+import re
+import asyncio
 from telethon.sync import TelegramClient
+import utils.chanData
+import utils.novice
 
 
 __all__ = ['TgLoginTool']
+
+
+class _NiUsersPhoneChoose():
+    def __init__(self, sessionDirPath: str = ''):
+        self._sessionDirPath = sessionDirPath
+        if not os.path.exists(sessionDirPath):
+            os.makedirs(sessionDirPath)
+
+        self._chanData = utils.chanData.ChanData()
+        if self._chanData.getSafe('.niUsers') == None:
+            self._chanData.set('.niUsers', {
+                'cemetery': [],
+                'bandInfos': [],
+                'bandList': [],
+                'lockList': [],
+            })
+        else:
+            self.updateBandInfo()
+
+        self.pickPhones = []
+
+    @utils.chanData.ChanData.dFakeLockSet(memberPath = '.niUsers', ysStore = True)
+    def updateBandInfo(self) -> dict:
+        niUsers = self._chanData.get('.niUsers')
+
+        bandInfos = niUsers['bandInfos']
+        bandInfosLength = len(bandInfos)
+        if bandInfosLength == 0:
+            return niUsers
+
+        bands = niUsers['bandList']
+        nowTimeMs = utils.novice.dateTimestamp(datetime.datetime.now())
+        for idx in range(bandInfosLength):
+            bandInfo = bandInfos[idx]
+            if bandInfo['bannedWaitTimeMs'] < nowTimeMs:
+                bandsIdx = utils.novice.indexOf(bands, bandInfo['id'])
+                del bandInfos[idx]
+                del bands[bandsIdx]
+
+        return niUsers
+
+    _regexSessionName = r'^telethon-(\d+).session$'
+
+    def _getUsablePhones(self) -> list:
+        phones = []
+        files = os.listdir(self._sessionDirPath)
+        for fileName in files:
+            if os.path.isfile(os.path.join(self._sessionDirPath, fileName)):
+                matchTgCode = re.search(self._regexSessionName, fileName)
+                if matchTgCode:
+                    phones.append(matchTgCode.group(1))
+        return phones
+
+    def getSessionPath(self, phoneNumber: str, noExt: bool = False):
+        path = self._sessionDirPath + '/telethon-' + phoneNumber
+        if noExt == False:
+            path += '.session'
+        return path
+
+    @utils.chanData.ChanData.dFakeLockSet(memberPath = '.niUsers', ysStore = True)
+    def _pushCemeteryData_chanData(self, phoneNumber: str) -> list:
+        niUsers = self._chanData.get('.niUsers')
+
+        locks = niUsers['lockList']
+        locksIdx = utils.novice.indexOf(locks, phoneNumber)
+        if locksIdx != -1:
+            del locks[locksIdx]
+
+        bands = niUsers['bandList']
+        bandsIdx = utils.novice.indexOf(bands, phoneNumber)
+        if bandsIdx != -1:
+            del bands[bandsIdx]
+            bandInfos = niUsers['bandInfos']
+            bandInfosLength = len(bandInfos)
+            for bandInfosIdx in range(bandInfosLength):
+                bandInfo = bandInfos[bandInfosIdx]
+                if bandInfo['id'] == phoneNumber:
+                    del bandInfos[bandInfosIdx]
+                    break
+
+        return niUsers
+
+    def pushCemeteryData(self, phoneNumber: str, err: Exception) -> bool:
+        sessionPath = self.getSessionPath(phoneNumber)
+        if not os.path.exists(sessionPath):
+            os.remove(sessionPath)
+            self._pushCemeteryData_chanData(phoneNumber)
+            return self._chanData.opt('jsonarrappend', '.niUsers.cemetery', {
+                'id': phoneNumber,
+                'message': '{} error: {}'.format(type(err), err),
+            })
+        return False
+
+    def pushBandData(self, phoneNumber: str, dt: datetime.datetime) -> bool:
+        bands = self._chanData.get('.niUsers.bandList')
+        if utils.novice.indexOf(bands, phoneNumber) == -1:
+            self._chanData.opt('jsonarrappend', '.niUsers.bandList', phoneNumber)
+            return self._chanData.opt('jsonarrappend', '.niUsers.bandInfos', {
+                'id': phoneNumber,
+                'bannedWaitDate': utils.novice.dateStringify(dt),
+                'bannedWaitTimeMs': utils.novice.dateTimestamp(dt)
+            })
+        return False
+
+    @utils.chanData.ChanData.dFakeLockSet(memberPath = '.niUsers.lockList', maxTimes = 1)
+    def _lockPhone_chanData(self, lockList: list) -> list:
+        return lockList
+
+    def lockPhone(self, phoneNumber: str) -> bool:
+        locks = self._chanData.get('.niUsers.lockList')
+        if utils.novice.indexOf(locks, phoneNumber) == -1:
+            locks.append(phoneNumber)
+            if self._lockPhone_chanData(locks):
+                self.pickPhones.append(phoneNumber)
+                return True
+        return False
+
+    async def pickPhone(self) -> str:
+        phones = self._getUsablePhones()
+        phonesLength = len(phones)
+        times = -1
+        while True:
+            times += 1
+            phoneNumber = phones[times % phonesLength]
+
+            niUsers = self._chanData.get('.niUsers')
+            bands = niUsers['bandList']
+            locks = niUsers['lockList']
+
+            if utils.novice.indexOf(bands, phoneNumber) != -1 \
+                    or utils.novice.indexOf(locks, phoneNumber) != -1:
+                continue
+
+            if self.lockPhone(phoneNumber):
+                return phoneNumber
+
+            await asyncio.sleep(1)
+
+    @utils.chanData.ChanData.dFakeLockSet(memberPath = '.niUsers.lockList')
+    def _releaseLockPhone_chanData(self, lockPhoneList: list) -> list:
+        locks = self._chanData.get('.niUsers.lockList')
+        for phoneNumber in lockPhoneList:
+            phoneIdx = utils.novice.indexOf(locks, phoneNumber)
+            if phoneIdx != -1:
+                del locks[phoneIdx]
+        return locks
+
+    def releaseLockPhone(self, phoneNumber: str = ''):
+        pickPhones = self.pickPhones
+        if phoneNumber == '':
+            self._releaseLockPhone_chanData(pickPhones)
+            pickPhones.clear()
+        else:
+            self._releaseLockPhone_chanData([phoneNumber])
+            phoneIdx = utils.novice.indexOf(pickPhones, phoneNumber)
+            if phoneIdx != -1:
+                del pickPhones[phoneIdx]
+        self._chanData.store()
 
 
 class TgLoginTool():
