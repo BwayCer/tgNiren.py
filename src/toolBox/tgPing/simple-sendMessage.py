f26c8d42548060db72b2745e4ea18e413674ca38
BwayCer <bwaycer@gmail.com>
2020-02-23 03:50:08 +0000

    chore: Hi 爆無法達到 999 的註解描述

--- src/toolBox/tgPing/simple-sendMessage.py
+++ src/toolBox/tgPing/simple-sendMessage.py
@@ -38,7 +38,12 @@ async def asyncRun(args: list, _dirpy: str, _dirname: str):
     ))
 
     print('-> 加入聊天室')
-    # 會受 telethon.errors.FloodWaitError 影響
+    # TODO
+    # `client.get_entity` 是一項昂貴的操作
+    #   https://docs.telethon.dev/en/latest/modules/client.html#telethon.client.users.UserMethods.get_entity
+    #   使用 `client.get_entity` 來解析用戶名是一項昂貴的操作，
+    #   若在短時間內請求 50 個用戶名則會引發 "FloodWaitError" 的錯誤。
+    # 在實際測試中，在短時間內請求相同的群組名也僅有 200 次的許可。 (2020.02.19 紀錄)
     inputPeer = await client.get_entity(toGroupPeer)
     if type(inputPeer) != telethon.types.User:
         print('--> telethon.functions.channels.JoinChannelRequest')
@@ -49,12 +54,12 @@ async def asyncRun(args: list, _dirpy: str, _dirname: str):
     print('-> 紫爆 Hi')
     pushErrMsg = ''
     async for idx in tgTool.iterLoopInterval(loopTimes, 1):
-        # TODO 不太會被封 需再測試才能確定 try cache 語句是否有錯誤
         try:
             readableIdx = idx + 1
             print('--> {}/{}: {} -> {}'.format(readableIdx, loopTimes, phoneNumber, toGroupPeer))
 
             print('---> telethon.functions.messages.SendMessageRequest')
+            # 在 1970 次請求後需等待 1324 秒 (2020.02.23 紀錄)
             await client(telethon.functions.messages.SendMessageRequest(
                 peer = toGroupPeer,
                 message = 'PurplePink {}{}'.format(
@@ -75,7 +80,7 @@ async def asyncRun(args: list, _dirpy: str, _dirname: str):
         except telethon.errors.FloodWaitError as err:
             print(utils.novice.sysTracebackException(ysHasTimestamp = True))
             waitTimeSec = err.seconds
-            # TODO 秒數待驗證
+            # TODO 印象中有短秒數的 FloodWaitError 錯誤
             print('FloodWaitError: {}'.format(err))
             if waitTimeSec < 720:
                 print('---> FloodWaitError: wait {} seconds.'.format(waitTimeSec))
